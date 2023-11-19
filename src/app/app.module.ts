4ed87cfc5f103a18d514e3341c1f000f738ec89a
Christiaan du Preez <christiaan.d@jobjack.co.za>
2023-11-20 00:28:08 +0200

    Commit it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { InstructionsComponent } from './instructions/instructions.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, InstructionsComponent],
+  declarations: [
+    AppComponent,
+    InstructionsComponent,
+    LandingPageComponent,
+    HeroSectionComponent,
+    NavBarComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
